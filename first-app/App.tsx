eb12b31542f11529facee5ce6c8dd83e462fcc98
Uhameed78 <hameedulla.umar@gmail.com>
2026-08-04 09:07:19 +0200

    Refactor navigation and state management in App component

--- first-app/App.tsx
+++ first-app/App.tsx
@@ -1,40 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button, Image, } from 'react-native';
-import { useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
+import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+  Home: undefined;
+  View: {
+    NameSend: string;
+    SurnameSend: string;
+  };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+type MainScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+type ViewDetailProps = NativeStackScreenProps<RootStackParamList, 'View'>;
 
 export default function App() {
-
-const Stack = createNativeStackNavigator();
-
   return (
     <NavigationContainer>
-
-<Stack.Navigator>
-
-  <Stack.Screen name= "Home" component ={ MainScreen}/>
-
-</Stack.Navigator>
-
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MainScreen} />
+        <Stack.Screen name="View" component={ViewDetails} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-
-  function MainScreen(){
-  const [Name, setName] = useState('')
-  const [Surname, setSurname] = useState('');
+function MainScreen({ navigation }: MainScreenProps) {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
 
   console.log("App works!");
 
   return (
-    
     <View>
       <Text style={styles.welcomeTxt}> Welcome to my app!</Text>
-      <Image style={styles.logo} source={require('./images/littleFella.png')}/>
+      <Image style={styles.logo} source={require('./images/littleFella.png')} />
 
- <View style={styles.inputFlex}>
+      <View style={styles.inputFlex}>
 
         <Text style={styles.headingTxt}> Please enter your name</Text>
 
@@ -60,15 +66,25 @@ const Stack = createNativeStackNavigator();
 
       </View>
 
-
-
-<Button
-  title="Add user"
-  onPress={() => (
-    console.log("Name: " + Name + " Surname: " + Surname)
-  )}
-/>
+      <Button
+        title="Add user"
+        onPress={() => {
+          navigation.navigate('View', { NameSend: name, SurnameSend: surname });
+          console.log("Name: " + name + " Surname: " + surname);
+        }}
+      />
       <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function ViewDetails({ route }: ViewDetailProps) {
+  const nameGet = route.params.NameSend;
+  const surnameGet = route.params.SurnameSend;
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> Name: {nameGet}; Surname: {surnameGet} </Text>
     </View>
   );
 }
@@ -80,16 +96,13 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 21,
     textAlign: 'center',
-    
-
   },
   headingTxt: {
     paddingTop: 69,
     color: 'purple',
     fontSize: 21,
     fontWeight: 'bold',
-    textAlign: 'auto', 
-    
+    textAlign: 'auto',
   },
   logo: {
     width: 200,
@@ -102,14 +115,11 @@ const styles = StyleSheet.create({
     color: 'black',
     fontSize: 21,
     fontWeight: 'bold',
-    textAlign: 'auto', 
+    textAlign: 'auto',
     borderBottomWidth: 1,
   },
   inputFlex: {
-   justifyContent: 'space-evenly',
-    marginTop:20 
-
-  }
-  
-  
+    justifyContent: 'space-evenly',
+    marginTop: 20,
+  },
 });
